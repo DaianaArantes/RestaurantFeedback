248b24da353fe123aad8d0ea908af56a7fb03439
DaianaArantes <daiana.arantes@hotmail.com>
2019-04-25 21:37:39 -0400

    added database file to project

---
+++ RestaurantFeedback/js/DAglobal.js
@@ -0,0 +1,126 @@
+// Calculation for Add Review
+function daAddOverallRating() {
+
+    var value1 = 0;
+    var value2 = 0;
+    var value3 = 0;
+
+    if(!isNaN(parseInt($("#daAddFoodQuality").val())) && (parseInt($("#daAddFoodQuality").val()) > 0 && parseInt($("#daAddFoodQuality").val()) <= 5)){
+        value1 = parseInt($("#daAddFoodQuality").val());
+    }else{
+        value1 = 0;
+    }
+
+    if(!isNaN(parseInt($("#daAddValue").val())) && (parseInt($("#daAddValue").val()) > 0 && parseInt($("#daAddValue").val()) <= 5)){
+        value2 = parseInt($("#daAddValue").val());
+    }else{
+        value2 = 0;
+    }
+
+    if(!isNaN(parseInt($("#daAddService").val())) && (parseInt($("#daAddService").val()) > 0 && parseInt($("#daAddService").val()) <= 5)){
+        value3 = parseInt($("#daAddService").val());
+    }else{
+        value3 = 0;
+    }
+
+
+    var total = (value1 + value2 + + value3) * 100/15;
+
+
+    $("#daAddOverallRating").val(total.toFixed(0) + "%");
+
+}
+
+// Calculation for Edit Review
+function daEditOverallRating() {
+
+    var value1 = 0;
+    var value2 = 0;
+    var value3 = 0;
+
+    if(!isNaN(parseInt($("#daEditFoodQuality").val())) && (parseInt($("#daEditFoodQuality").val()) > 0 && parseInt($("#daEditFoodQuality").val()) <= 5)){
+        value1 = parseInt($("#daEditFoodQuality").val());
+    }else{
+        value1 = 0;
+    }
+
+    if(!isNaN(parseInt($("#daEditValue").val())) && (parseInt($("#daEditValue").val()) > 0 && parseInt($("#daEditValue").val()) <= 5)){
+        value2 = parseInt($("#daEditValue").val());
+    }else{
+        value2 = 0;
+    }
+
+    if(!isNaN(parseInt($("#daEditService").val())) && (parseInt($("#daEditService").val()) > 0 && parseInt($("#daEditService").val()) <= 5)){
+        value3 = parseInt($("#daEditService").val());
+    }else{
+        value3 = 0;
+    }
+
+
+    var total = (value1 + value2 + + value3) * 100/15;
+
+
+    $("#daEditOverallRating").val(total.toFixed(0) + "%");
+
+}
+
+function daBtnSave_click() {
+    daValidate_daAddForm();
+}
+
+function daBtnUpdate_click() {
+    daValidate_daEditForm();
+}
+
+function daSettingsBtnSave_click() {
+    daInitStorage();
+}
+
+function init() {
+    $("#daAddFoodQuality").on("keyup", daAddOverallRating);
+    $("#daAddValue").on("keyup", daAddOverallRating);
+    $("#daAddService").on("keyup", daAddOverallRating);
+
+
+    $("#daEditFoodQuality").on("keyup", daEditOverallRating);
+    $("#daEditValue").on("keyup", daEditOverallRating);
+    $("#daEditService").on("keyup", daEditOverallRating);
+
+    $("#daBtnSave").on("click", daBtnSave_click);
+    $("#daBtnUpdate").on("click", daBtnUpdate_click);
+
+    $("#daSettingsBtnSave").on("click", daSettingsBtnSave_click);
+}
+
+$(document).ready(function (){
+
+    init();
+
+    //Hide or show
+    $("#daAddRatings").change(function(){
+
+        var isChecked = $(this).is(':checked');
+
+        if (isChecked){
+            $("#daAddRating").show();
+        }else{
+            $("#daAddRating").hide();
+        }
+    });
+
+    $("#daEditRatings").change(function(){
+
+        var isChecked = $(this).is(':checked');
+
+        if (isChecked){
+            $("#daEditRating").show();
+        }else{
+            $("#daEditRating").hide();
+        }
+    });
+});
+
+function daInitStorage(){
+    localStorage.setItem("DefaultEmail", $("#daSettingsEmail").val());
+    alert("Default Reviewer email saved");
+}
---
+++ RestaurantFeedback/js/DAutil.js
@@ -0,0 +1,147 @@
+function daValidate_daAddForm(){
+    var form = $("#daAddForm");
+
+
+    form.validate({
+        rules: {
+            daAddBusinessName: {
+                required: true,
+                rangelength: [2,20]
+
+            },
+            daAddReviewerEmail:{
+                required: true,
+                emailcheck: true
+            },
+            daAddReviewDate: {
+                required: true
+            },
+            daAddFoodQuality: {
+                required: true,
+                min: 0,
+                max: 5
+            },
+            daAddValue: {
+                required: true,
+                min: 0,
+                max: 5
+            },
+            daAddService: {
+                required: true,
+                min: 0,
+                max: 5
+            }
+
+
+        },
+        messages: {
+            daAddBusinessName: {
+                required: "You must enter name",
+                rangelength: "Length must be 2-20 characters long"
+            },
+            daAddReviewerEmail:{
+                required: "You must enter an email",
+                emailcheck: "Please enter a valid email address."
+            },
+            daAddReviewDate: {
+                required: "Review date is required"
+            },
+            daAddFoodQuality: {
+                required: "You must enter a rating",
+                min: "Value must be 0-5",
+                max: "Value must be 0-5"
+            },
+            daAddValue: {
+                required: "You must enter a rating",
+                min: "Value must be 0-5",
+                max: "Value must be 0-5"
+            },
+            daAddService: {
+                required: "You must enter a rating",
+                min: "Value must be 0-5",
+                max: "Value must be 0-5"
+            }
+        }
+    });
+
+
+    return form.valid();
+}
+
+
+
+function daValidate_daEditForm(){
+    var form = $("#daEditForm");
+
+
+    form.validate({
+        rules: {
+            daEditBusinessName: {
+                required: true,
+                rangelength: [2,20]
+            },
+            daEditReviewerEmail:{
+                required: true,
+                emailcheck: true
+            },
+            daEditReviewDate: {
+                required: true
+            },
+            daEditFoodQuality: {
+                required: true,
+                min: 0,
+                max: 5
+            },
+            daEditValue: {
+                required: true,
+                min: 0,
+                max: 5
+            },
+            daEditService: {
+                required: true,
+                min: 0,
+                max: 5
+            }
+        },
+        messages: {
+            daEditBusinessName: {
+                required: "You must enter name",
+                rangelength: "Length must be 2-20 characters long"
+            },
+            daEditReviewerEmail:{
+                required: "You must enter an email",
+                emailcheck: "Please enter a valid email address."
+            },
+            daEditReviewDate: {
+                required: "Review date is required"
+            },
+            daEditFoodQuality: {
+                required: "You must enter a rating",
+                min: "Value must be 0-5",
+                max: "Value must be 0-5"
+            },
+            daEditValue: {
+                required: "You must enter a rating",
+                min: "Value must be 0-5",
+                max: "Value must be 0-5"
+            },
+            daEditService: {
+                required: "You must enter a rating",
+                min: "Value must be 0-5",
+                max: "Value must be 0-5"
+            }
+        }
+    });
+
+    return form.valid();
+}
+
+
+// Email check custom validation
+jQuery.validator.addMethod("emailcheck",
+    function(value, element){
+        var regex = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+        return this.optional(element) || regex.test(value);
+    },
+    "Custom email checker");
+
